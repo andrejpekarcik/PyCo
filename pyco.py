dd97e4b12e4a9b35dfd107d57e52871d9d5a61ce
andrejpekarcik <andrejpekarcik@gmail.com>
2018-09-30 14:05:22 +0200

    male

--- pyco.py
+++ pyco.py
@@ -15,9 +15,6 @@ while True:
     a= uart.readline()
     if a:
         a = str(a).replace('b','').replace('\'',s'')
-        ##print (a)
-        #
-        #
         if a[0] == '$' and a[len(a)-4:] == "\\r\\n":
             a = a[:len(a)-4]
             print (a)
