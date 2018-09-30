6e41f6f1369e9281da0dd0aea365a23c373b083e
andrejpekarcik <andrejpekarcik@gmail.com>
2018-09-30 14:32:38 +0200

    male

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PyCo
 PyCo
+  GPy + GPS
+
+  
--- pyco.py
+++ pyco.py
@@ -19,17 +19,3 @@ while True:
         if a[0] == '$' and a[len(a)-4:] == "\\r\\n":
             a = a[:len(a)-4]
             print (a,NMEA_veta(a))
-
-#
-# Overi ci NMEA veta a checksum sedia, vracia True, False
-#
-def NMEAchecksum(NMEA_veta):
-
-    if NMEA_veta.count('*') != 1 and NMEA_veta[len(NMEA_veta)-3]:
-        return False
-
-    # ak je na zaciatku $ tak sa odstrani
-    if NMEA_veta[0] == "$":
-        NMEA_veta = NMEA_veta.replace('$','')
-
-    return True
