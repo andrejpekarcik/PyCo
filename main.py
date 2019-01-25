9f19b50ca3bfd11bc39cf20a38a44fc9ab19cade
andrejpekarcik <andrejpekarcik@gmail.com>
2019-01-25 18:54:18 +0100

    .

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ import time
 # Kolko ma hlboko spat medzi cas medzi meraniami v sekundach
 cas_medzi_meraniami = 3600
 
+# Pauza pred meranim GPS
+pauza_pred_GPS = 0.1
+
 # Ako dlho sa ma pokusat precitat GPS v sekundach
 max_cas_pre_gps = 300
 
@@ -38,21 +41,31 @@ pycom.rgbled(pyco.zlta)
 print ('--SiPy------------------------------------------------------------------')
 
 while True:
+
     lap = stopky_pre_GPS.read()
-    time.sleep(0.1)
-    if (lap == 0.0):
+    time.sleep(pauza_pred_GPS)
+
+    if (lap == 0):
         print ("sama nula")
+        stopky_pre_GPS.start()
+
+    if (lap > max_cas_pre_gps):
+        stopky_pre_GPS.stop()
+        ds.go_to_sleep(cas_medzi_meraniami)
+
     print (lap)
+
     raw_gps_data = uart.readline()
     NMEA , NMEA_stav = pyco.NMEAchecksum (raw_gps_data)
+
     if NMEA_stav:
         sirka, dlzka, poloha_stav = pyco.NMEA_poloha (NMEA)
         print ("Poloha stav:",poloha_stav)
         if poloha_stav:
             pycom.rgbled(pyco.zelena)
-            time.sleep(5)
             pyco.sigfox_posli(sirka)
             pyco.sigfox_posli(dlzka)
-            ds.go_to_sleep(120)
+            stopky_pre_GPS.stop()
+            ds.go_to_sleep(cas_medzi_meraniami)
         else:
             pycom.rgbled(pyco.modra)
