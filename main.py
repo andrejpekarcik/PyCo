cb70eb737c4cd84b1b827323d980a4f0dde40f37
andrejpekarcik <andrejpekarcik@gmail.com>
2019-01-25 18:04:32 +0100

    .

--- main.py
+++ main.py
@@ -4,18 +4,26 @@ import sys
 import pycom
 from deepsleep import DeepSleep
 import deepsleep
+from machine import Timer
+import time
 
 from network import Sigfox
 import socket
 from machine import UART
 import time
 
+# Kolko ma hlboko spat medzi cas medzi meraniami v sekundach
+cas_medzi_meraniami = 3600
+
+# Ako dlho sa ma pokusat precitat GPS v sekundach
+max_cas_pre_gps = 300
+
 print ('-SiPy start------------------------------------------------------------')
 
 ds = DeepSleep()
+stopky_pre_GPS = Timer.Chrono()
 
 sigfox = pyco.sigfox_init()
-pyco.sigfox_posli ('9999')
 
 # Vypnut LED
 pycom.heartbeat(False)
@@ -30,6 +38,11 @@ pycom.rgbled(pyco.zlta)
 print ('--SiPy------------------------------------------------------------------')
 
 while True:
+    lap = stopky_pre_GPS.read()
+    time.sleep(0.1)
+    if (lap == 0.0):
+        print ("sama nula")
+    print (lap)
     raw_gps_data = uart.readline()
     NMEA , NMEA_stav = pyco.NMEAchecksum (raw_gps_data)
     if NMEA_stav:
